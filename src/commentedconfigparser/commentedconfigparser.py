06d7ec6bd068a90a29dc08adbd672f5603ccaa09
Preocts <preocts@gmail.com>
2022-11-02 18:59:56 -0400

    Helpers for taking apart lines for key values

--- src/commentedconfigparser/commentedconfigparser.py
+++ src/commentedconfigparser/commentedconfigparser.py
@@ -12,7 +12,8 @@ from pathlib import Path
 from typing import Union
 
 StrOrBytesPath = Union[str, bytes, "PathLike[str]", "PathLike[bytes]"]
-COMMENT = re.compile(r"\s*[#|;]")
+COMMENT_PTN = re.compile(r"\s*[#|;]")
+KEY_PTN = re.compile("(.+?)[=|:]")
 
 
 class CommentedConfigParser(ConfigParser):
@@ -35,9 +36,21 @@ class CommentedConfigParser(ConfigParser):
 
     def _fileload(self, filepath: str) -> str | None:
         """Load a file if it exists."""
+        # TODO: This will need to handle StrOrBytesPath and file-likes
         path = Path(filepath)
         return path.read_text() if path.exists() else None
 
     def _is_comment_or_empty(self, line: str) -> bool:
         """True if the line is a valid ini comment."""
-        return bool(COMMENT.search(line)) or not bool(re.sub(r"\s*", "", line))
+        return bool(COMMENT_PTN.search(line)) or not bool(re.sub(r"\s*", "", line))
+
+    def _get_key(self, line: str) -> str:
+        """
+        Return the key of a line trimmed of leading/trailing whitespace.
+
+        Respects both `=` and `:` delimiters, uses which happens first. If
+        the line contains neither, the entire line is returned.
+        """
+        # Find which of the two assigment delimiters is used first
+        matches = KEY_PTN.match(line)
+        return matches.group(1).strip() if matches else line.strip()
--- tests/commentconfig_test.py
+++ tests/commentconfig_test.py
@@ -1,7 +1,37 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import pytest
 from commentedconfigparser.commentedconfigparser import CommentedConfigParser
+
+# import json
+
+CONFIG_W_COMMENTS = Path("tests/withcomments.ini").read_text()
+EXPECTED_MAP = {
+    "@@header": [],
+    "[DEFAULT]": [
+        "# This value has some meaning to someone",
+    ],
+    "foo": [
+        "# Make sure to add this when you need it",
+    ],
+    "logging": [
+        "; This is a comment as well",
+        "# so we need to track all of them",
+        "    ; and many could be between things",
+    ],
+    "[NEW SECTION]": [
+        "# Another comment",
+    ],
+    "@@footer": [
+        "",
+    ],
+}
+
+
+def test_assert_class_var_is_always_empty() -> None:
+    assert CommentedConfigParser._comment_map == {}
 
 
 @pytest.mark.parametrize(
@@ -23,6 +53,30 @@ def test_is_comment_or_empty(line: str, expected: bool) -> None:
     result = cc._is_comment_or_empty(line)
 
     assert result is expected
+
+
+@pytest.mark.parametrize(
+    ("line", "expected"),
+    (
+        ("key = value", "key"),
+        ("key=value", "key"),
+        ("key=:value", "key"),
+        ("key:=value", "key"),
+        ("key : value", "key"),
+        ("key:value", "key"),
+        ("\tkey with spaces=value", "key with spaces"),
+        ("\tkey: value with = in it", "key"),  # both delimiters
+        ("\tkey=val:ues", "key"),  # both delimiter equal spacing
+        ("\t[SECTION name]", "[SECTION name]"),
+        ("", ""),
+    ),
+)
+def test_get_line_key(line: str, expected: str) -> None:
+    cc = CommentedConfigParser()
+
+    result = cc._get_key(line)
+
+    assert result == expected
 
 
 def test_regression_read_reads() -> None:
