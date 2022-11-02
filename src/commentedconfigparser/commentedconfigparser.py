da33f4a1e397419507a8ac146db629a5cee145e4
Preocts <preocts@gmail.com>
2022-11-02 14:24:02 -0400

    Setup our subclass with the read methods we'll be using

--- src/commentedconfigparser/commentedconfigparser.py
+++ src/commentedconfigparser/commentedconfigparser.py
@@ -5,28 +5,40 @@ Custom ConfigParser class that preserves comments when writing a loaded config o
 from __future__ import annotations
 
 import re
+from collections.abc import Iterable
+from configparser import ConfigParser
+from os import PathLike
 from pathlib import Path
+from typing import Union
 
+StrOrBytesPath = Union[str, bytes, "PathLike[str]", "PathLike[bytes]"]
 COMMENT = re.compile(r"\s*[#|;]")
 
 
-class CommentConfig:
+class CommentedConfigParser(ConfigParser):
     """Custom ConfigParser that preserves comments when writing a loaded config out."""
 
-    def __init__(self, filepath: str | None = None) -> None:
-        """Load file if provided."""
-        self._comment_map: dict[str, list[str]] = {}
+    _comment_map: dict[str, list[str]] | None = None
 
-        if filepath:
-            self.load(filepath)
+    def read(
+        self,
+        filenames: StrOrBytesPath | Iterable[StrOrBytesPath],
+        encoding: str | None = None,
+    ) -> list[str]:
+        return super().read(filenames, encoding)
 
-    def load(self, filepath: str) -> None:
-        """
-        Load a file. Will replace any previously loaded file.
+    def read_file(self, f: Iterable[str], source: str | None = None) -> None:
+        return super().read_file(f, source)
 
-        Args:
-            filepath: path to target file, will raise if missing.
-        """
+    def read_string(self, string: str, source: str = "<string>") -> None:
+        return super().read_string(string, source)
+
+    def _fileload(self, filepath: str) -> None:
+        """Load a file. Will replace any previously loaded file."""
+        path = Path(filepath)
+        if not path.exists():
+            return
+
         content = Path(filepath).read_text()
 
         for content_line in content.split("\n"):
--- tests/commentconfig_test.py
+++ tests/commentconfig_test.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import pytest
-from commentedconfigparser.commentedconfigparser import CommentConfig
+from commentedconfigparser.commentedconfigparser import CommentedConfigParser
 
 
 @pytest.mark.parametrize(
@@ -17,8 +17,32 @@ from commentedconfigparser.commentedconfigparser import CommentConfig
     ),
 )
 def test_is_comment(line: str, expected: bool) -> None:
-    cc = CommentConfig()
+    cc = CommentedConfigParser()
 
     result = cc._is_comment(line)
 
     assert result is expected
+
+
+def test_regression_read_reads() -> None:
+    cc = CommentedConfigParser()
+
+    read = cc.read(["tests/withcomments.ini", "notfoundatall.ini"])
+
+    assert len(read) == 1
+
+
+def test_regression_read_string_loads_normally() -> None:
+    cc = CommentedConfigParser()
+
+    cc.read_string("[TEST]\ntest=pass")
+
+    assert cc.get("TEST", "test") == "pass"
+
+
+def test_regression_read_dict_loads_normally() -> None:
+    cc = CommentedConfigParser()
+
+    cc.read_dict({"TEST": {"test": "pass"}})
+
+    assert cc.get("TEST", "test") == "pass"
